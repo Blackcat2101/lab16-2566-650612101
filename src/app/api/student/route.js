bd57c7e1005d07ddf0666bf19dff81aabff9992b
santhod <santhod11111@gmail.com>
2023-09-04 16:11:41 +0700

    f1

--- src/app/api/student/route.js
+++ src/app/api/student/route.js
@@ -1,5 +1,6 @@
 import { DB } from "@/app/libs/DB";
 import {
+  zStudentDeleteBody,
   zStudentGetParam,
   zStudentPostBody,
   zStudentPutBody,
@@ -30,7 +31,9 @@ export const GET = async (request) => {
     filtered = filtered.filter((std) => std.program === program);
   }
 
-  //filter by student id here
+  if (studentId !== null) {
+    filtered = filtered.filter((std) => std.studentId === studentId);
+  }
 
   return NextResponse.json({ ok: true, students: filtered });
 };
@@ -98,8 +101,34 @@ export const PUT = async (request) => {
 
 export const DELETE = async (request) => {
   //get body and validate it
+  const body = await request.json();
 
-  //check if student id exist
+  const parseResult = zStudentDeleteBody.safeParse(body);
+  if (parseResult.success === false) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Student Id must contain 9 characters",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const foundIndex = DB.students.findIndex(
+    (std) => std.studentId === body.studentId
+  );
+  if (foundIndex === -1) {
+    return NextResponse.json(
+      { ok: false, message: "Student ID does not exist" },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  DB.students = DB.students.filter((std) => std.studentId !== body.studentId);
 
   //perform removing student from DB. You can choose from 2 choices
   //1. use array filter method
@@ -110,6 +139,6 @@ export const DELETE = async (request) => {
 
   return NextResponse.json({
     ok: true,
-    message: `Student Id xxx has been deleted`,
+    message: `Student Id ${body.studentId} has been deleted`,
   });
 };
